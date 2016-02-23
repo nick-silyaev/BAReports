9311555ba368d48171991fc3a840432e99805cd0
Nick Silyaev <nickolay.silyaev@gmail.com>
2016-02-23 22:36:59 +0300

    adding d3-tip as a module. Adding tips to Timeline chart

--- BACharts/js/app/Analytics.js
+++ BACharts/js/app/Analytics.js
@@ -9,7 +9,8 @@
 
   // setup dependency injection
   angular.module('d3', []);
+  angular.module('tip', ['d3']);
   angular.module('Analytics.controllers', []);
-  angular.module('Analytics.directives', ['d3']);
+  angular.module('Analytics.directives', ['d3', 'tip']);
 
 }());
--- BACharts/js/directives/baTimeline.js
+++ BACharts/js/directives/baTimeline.js
@@ -2,21 +2,29 @@
     'use strict';
 
     angular.module('Analytics.directives')
-        .directive('baTimeline', ['d3', function(d3) {
+        .directive('baTimeline', ['d3', 'tip', function(d3, tip) {
             return {
                 restrict: 'EA',
                 scope: {
                     data: "=",
+                    settings: "=",
                     label: "@",
                     onClick: "&"
                 },
                 link: function(scope, iElement, iAttrs) {
 
+                    var tip = d3.tip()
+                        .attr('class', 'd3-tip')
+                        .offset([-20, 0])
+                        .html(function(d) {
+                            return "<strong>Date: </strong><span>" +  d[0]+ "</span><br><strong>Page Views: </strong><span>" + d[1]+ "</span><br><strong>Activities: </strong><span>" + d[2]+ "</span>";
+                        });
 
                     var svg = d3.select(iElement[0])
                         .append("svg")
                         .attr("class", "analytics-timeline")
-                        .attr("width", "100%");
+                        .attr("width", "100%")
+                        .call(tip);
 
                     // on window resize, re-render d3 canvas
                     window.onresize = function() {
@@ -25,7 +33,7 @@
                     scope.$watch(function(){
                             return angular.element(window)[0].innerWidth;
                         }, function(){
-                            return scope.render(scope.data);
+                            return scope.render(scope.data, scope.settings );
                         }
                     );
 
@@ -35,25 +43,28 @@
                     //}, true);
 
                     // define render function
-                    scope.render = function(data){
+                    scope.render = function(data, settings){
                         // remove all previous items before render
                         svg.selectAll("*").remove();
                         // setup variables
                         var width, height, margin, innerHeight;
                         //containing element width
                         width = d3.select(iElement[0])[0][0].offsetWidth;
-                        //svg height to 1/10 of width
-                        height = 50;
+                        //svg height. default is 50
+                        height = typeof(settings.height) !== "undefined" ? settings.height : 50;
+                        //margins
                         margin = {top: 10, right: 20, bottom: 10, left: 20};
                         innerHeight = height - margin.top - margin.bottom
                         // set the height based on the calculations above
                         svg.attr('height', height);
 
-                        var timeFrame = 6;
+                        // months to display on the timeline. default is 6
+                        var months = typeof(settings.months) !== "undefined" ? settings.months : 6;
                         //now
                         var endDate = new Date();
                         // get last six months
-                        var startDate = d3.time.month.offset(endDate, -timeFrame);
+                        var startDate = d3.time.month.offset(endDate, -months);
+
 
                         var xScale = d3.time.scale()
                             .domain([startDate, endDate])
@@ -64,14 +75,6 @@
                             .ticks(d3.time.months)
                             .tickSize(0,0)
                             .tickFormat(d3.time.format(""));
-
-                        //var tip = d3.tip()
-                        //    .attr('class', 'timeline-tip')
-                        //    .offset([-10, 0])
-                        //    .html(function(d) {
-                        //        return "<strong>Date: </strong><span>" + d[0] + "</span><br><strong>Page Views: </strong><span>" + d[1] + "</span><strong>Activities: </strong><span>" + d[2] + "</span>";
-                        //    })
-
 
                         svg.append("g")
                             .attr("class", "x axis")
@@ -109,7 +112,7 @@
 
                         var valueScale = innerHeight/maxValue;
 
-                        var dateFormat = d3.time.format("%Y-%m-%dT%H:%M:%S");
+                        var dateFormat = d3.time.format("%Y-%m-%d");
 
                         svg.append("g")
                             .attr("class", "centers")
@@ -131,118 +134,18 @@
                             .selectAll("circle")
                             .data(data.values)
                             .enter().append("circle")
-                            .attr("class", function(d){
-                                if(d[2] > 0) {return "circle actions"}
-                                else {return "circle"}
-                            })
-                            .attr("cx", function(d){
-                                return xScale(dateFormat.parse(d[0]))
-                            })
+                            .on('mouseover',  function(d) {tip.show(d)})
+                            .on('mouseout',  function(d) {tip.hide(d)})
+                            .attr("class", function(d){return d[2] > 0 ? "circle actions" : "circle"})
+                            .attr("cx", function(d){return xScale(dateFormat.parse(d[0]))})
                             .attr("cy", height/2)
                             .attr("r", 1)
-                            .transition()
-                            .duration(1500)
-                            .ease('elastic').delay( function(d, i){return Math.random()*500})
-                            .attr("r", function(d){return d[1]*valueScale/2})
-                            .on("mouseover", function(){})
-                            //.on("mousemove", function(){
-                            //    return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
-                            //})
-                            .on("mouseout", function(){});
+                            .transition().duration(1500)
+                                .ease('elastic').delay( function(d, i){return Math.random()*500})
+                                .attr("r", function(d){return d[1]*valueScale/2})
+                                .attr("tooltip-append-to-body", true)
+                                .attr("tooltip", function(d){return d[0];});
 
-
-
-
-
-                        //var xScale = d3.scale.ordinal()
-                        //    .domain( data.values.map(function(d){return d[0]}))
-                        //    .rangePoints([margin.left, width - margin.right]);
-                        //
-                        //var yScale = d3.scale.linear()
-                        //    .domain([0,100])
-                        //    .range([height - margin.bottom , margin.top]);
-                        //
-                        //var xAxis = d3.svg.axis()
-                        //    .scale(xScale);
-                        //
-                        //var yAxis = d3.svg.axis()
-                        //    .scale(yScale)
-                        //    .orient("left")
-                        //    .ticks(5).tickFormat(function(d){return d+ "%"});
-                        //
-                        //
-                        //var studentsStart = d3.svg.area()
-                        //    .x(function(d, i) { return xScale(d[0]) })
-                        //    .y0(height-margin.bottom)
-                        //    .y1(height-margin.bottom);
-                        //
-                        //var studentsEnd = d3.svg.area()
-                        //    .x(function(d, i) { return xScale(d[0]) })
-                        //    .y0(height-margin.bottom)
-                        //    .y1(function(d) { return yScale(d[1])});
-                        //
-                        //var teachersStart = d3.svg.area()
-                        //    .x(function(d, i) { return xScale(d[0]) })
-                        //    .y0(height-margin.bottom)
-                        //    .y1(height-margin.bottom);
-                        //
-                        //var teachersEnd = d3.svg.area()
-                        //    .x(function(d, i) { return xScale(d[0]) })
-                        //    .y0(height-margin.bottom)
-                        //    .y1(function(d) { return yScale(d[2]) });
-                        //
-                        //var xAxis = svg.append("g")
-                        //    .attr("class", "x axis")
-                        //    .attr("transform", "translate(0," + (height - margin.bottom) + ")")
-                        //    .call(xAxis);
-                        //xAxis.selectAll("path, line")
-                        //    .style("display", "none");
-                        //
-                        //xAxis.selectAll("text")
-                        //    .style("fill", "#445566")
-                        //    .style("font-size", "12px");
-                        //
-                        //var yAxis = svg.append("g")
-                        //    .attr("class", "y axis")
-                        //    .attr("transform", "translate(" + margin.left + ", 0)")
-                        //    .call(yAxis)
-                        //
-                        //yAxis.selectAll("path, line")
-                        //    .style("display", "none");
-                        //
-                        //yAxis.selectAll("text")
-                        //    .style("fill", "#445566")
-                        //    .style("font-size", "12px");
-                        //
-                        //svg.append("path")
-                        //    .datum(data.values)
-                        //    .attr("class", "area")
-                        //    .attr("d", studentsStart)
-                        //    .style("fill", '#3598dc' )
-                        //    .transition().duration(1000).attr("d", studentsEnd);
-                        //
-                        //svg.append("path")
-                        //    .datum(data.values)
-                        //    .attr("class", "area")
-                        //    .attr("d", teachersStart)
-                        //    .style("fill", '#ea5d4b' )
-                        //    .on('mouseover', function(d){
-                        //        //mouseover function
-                        //    })
-                        //    .transition().duration(1000).delay(1000).attr("d", teachersEnd);
-                        //
-                        //
-                        ////draw horisontal grid lines
-                        //svg.selectAll("line.y")
-                        //    .data(yScale.ticks(5))
-                        //    .enter().append("line")
-                        //    .attr("class", "y")
-                        //    .attr("x1", margin.left)
-                        //    .attr("x2", width-margin.right)
-                        //    .attr("y1", yScale)
-                        //    .attr("y2", yScale)
-                        //    //.attr("transform", "translate(0, "+ (-margin.top) +")")
-                        //    .style("stroke", "#ebebeb");
 
                     };
                 }
